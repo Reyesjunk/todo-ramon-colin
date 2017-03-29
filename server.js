d989dd50cb40307c4d7c6caf5f1d9fcb71a79054
cfv7 <hq@colin.red>
2017-03-29 16:01:42 -0400

    added CORS and simple GET

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.header({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+  });  
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  res.json({})
+});
+
+
 app.listen(process.env.PORT || 8080);
 
 exports.app = app;
